57da7e9679b3a044632a535ba6965fa04e2c845d
Brian Kubes <briankubes@gmail.com>
2020-10-02 09:41:55 -0700

    almost done with form test

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -18,6 +18,8 @@ const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [values, setValues, handleChanges] = useForm(initialValue);
 
+  //I put handleChanges in useForm.js and removed all 's'
+
   // const handleChanges = (e) => {
   //   setValues({ ...values, [e.target.name]: e.target.value });
   // };
@@ -31,41 +33,59 @@ const CheckoutForm = (props) => {
     <>
       <form onSubmit={handleSubmit}>
         <h2>Checkout Form</h2>
-        <label>
+        <label htmlFor="firstName">
           First Name:
           <input
             name="firstName"
+            id="firstName"
             value={values.firstName}
             onChange={handleChanges}
           />
         </label>
-        <label>
+        <label htmlFor="lastName">
           Last Name:
           <input
             name="lastName"
+            id="lastName"
             value={values.lastName}
             onChange={handleChanges}
           />
         </label>
-        <label>
+        <label htmlFor="address">
           Address:
           <input
             name="address"
+            id="address"
             value={values.address}
             onChange={handleChanges}
           />
         </label>
-        <label>
+        <label htmlFor="city">
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input
+            name="city"
+            id="city"
+            value={values.city}
+            onChange={handleChanges}
+          />
         </label>
-        <label>
+        <label htmlFor="state">
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input
+            name="state"
+            id="state"
+            value={values.state}
+            onChange={handleChanges}
+          />
         </label>
-        <label>
+        <label htmlFor="zip">
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input
+            name="zip"
+            id="zip"
+            value={values.zip}
+            onChange={handleChanges}
+          />
         </label>
         <button>Checkout</button>
       </form>
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,34 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  render(<CheckoutForm />);
 
-test("form shows success message on submit with form details", () => {});
+  const header = screen.getByText(/Checkout Form/i);
+  expect(header).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", () => {
+  render(<CheckoutForm />);
+
+  const firstName = screen.getByLabelText(/first name/i);
+  const lastName = screen.getByLabelText(/last name/i);
+  const address = screen.getByLabelText(/address/i);
+  const city = screen.getByLabelText(/city/i);
+  const state = screen.getByLabelText(/state/i);
+  const zip = screen.getByLabelText(/zip/i);
+  const checkoutButton = screen.getByRole("button", /Checkout/i);
+
+  fireEvent.change(firstName, { target: { value: "Brian" } });
+  fireEvent.change(lastName, { target: { value: "Kubes" } });
+  fireEvent.change(address, { target: { value: "3800 Country Club" } });
+  fireEvent.change(city, { target: { value: "Los Angeles" } });
+  fireEvent.change(state, { target: { value: "CA" } });
+  fireEvent.change(zip, { target: { value: "90019" } });
+  fireEvent.click(checkoutButton);
+
+  // You have ordered some plants!
+});
